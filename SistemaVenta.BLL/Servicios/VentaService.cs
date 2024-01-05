96e17e50f587e589f0dfc93582fa244b67c798bd
Daniel Perez <daniel.perezh@usantoto.edu.co>
2024-01-04 21:40:12 -0500

    Primera version Api

---
+++ SistemaVenta.BLL/Servicios/VentaService.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.BLL.Servicios.Contrato;
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DTO;
+using SistemaVenta.Model;
+
+namespace SistemaVenta.BLL.Servicios
+{
+    public class VentaService: IVentaService
+    {
+        private readonly IVentaRepository _ventaRepositorio;
+        private readonly IGenericRepository<DetalleVenta> _detalleVentaRepositorio;
+        private readonly IMapper _mapper;
+
+        public VentaService(IVentaRepository ventaRepositorio,
+            IGenericRepository<DetalleVenta> detalleVentaRepositorio,
+            IMapper mapper)
+        {
+            _ventaRepositorio = ventaRepositorio;
+            _detalleVentaRepositorio = detalleVentaRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<VentaDTO> Registrar(VentaDTO modelo)
+        {
+            try
+            {
+                var ventaGenerada = await _ventaRepositorio.Registrar(_mapper.Map<Venta>(modelo));
+                if (ventaGenerada == null)
+                    throw new TaskCanceledException("No se pudo crear");
+
+                return _mapper.Map<VentaDTO>(ventaGenerada);
+            } catch
+            {
+                throw;
+            }
+        }
+        public async Task<List<VentaDTO>> Historial(string buscarPor, string numeroVenta, string fechaInicio, string fechaFin)
+        {
+            IQueryable<Venta> query = await _ventaRepositorio.Consultar();
+            var listaResultado = new List<Venta>();
+            try
+            {
+                if(buscarPor == "fecha")
+                {
+                    DateTime fecha_inicio = DateTime.ParseExact(fechaInicio, "dd/MM/yyyy", new CultureInfo("es-PE"));
+                    DateTime fecha_fin = DateTime.ParseExact(fechaFin, "dd/MM/yyyy", new CultureInfo("es-PE"));
+
+                    listaResultado = await query.Where(v =>
+                    v.FechaRegistro.Value.Date >= fecha_inicio.Date &&
+                    v.FechaRegistro.Value.Date <= fecha_fin.Date
+                    ).Include(dv => dv.DetalleVenta)
+                    .ThenInclude(p => p.IdProductoNavigation)
+                    .ToListAsync();
+                }
+                else
+                {
+                    listaResultado = await query.Where(v =>
+                    v.NumeroDocumento == numeroVenta
+                    ).Include(dv => dv.DetalleVenta)
+                    .ThenInclude(p => p.IdProductoNavigation)
+                    .ToListAsync();
+                }
+            } catch
+            {
+                throw;
+            }
+
+            return _mapper.Map<List<VentaDTO>>(listaResultado);
+
+        }
+
+        public async Task<List<ReporteDTO>> Reporte(string fechaInicio, string fechaFin)
+        {
+            IQueryable<DetalleVenta> query = await _detalleVentaRepositorio.Consultar();
+            var listaResultado = new List<DetalleVenta>();
+            try 
+            {
+                DateTime fecha_inicio = DateTime.ParseExact(fechaInicio, "dd/MM/yyyy", new CultureInfo("es-PE"));
+                DateTime fecha_fin = DateTime.ParseExact(fechaFin, "dd/MM/yyyy", new CultureInfo("es-PE"));
+
+                listaResultado = await query
+                    .Include(p => p.IdProductoNavigation)
+                    .Include(v => v.IdVentaNavigation)
+                    .Where(dv =>
+                    dv.IdVentaNavigation.FechaRegistro.Value.Date >= fecha_inicio &&
+                    dv.IdVentaNavigation.FechaRegistro.Value.Date <= fecha_fin
+                    ).ToListAsync();
+            } catch 
+            {
+                throw;
+            }
+
+            return _mapper.Map<List<ReporteDTO>>(listaResultado);
+        }
+    }
+}
---
+++ SistemaVenta.DAL/Repositorios/GenericRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.DAL.Repositorios.Contrato;
+using SistemaVenta.DAL.DBContext;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaVenta.DAL.Repositorios
+{
+    public class GenericRepository<TModel>: IGenericRepository<TModel> where TModel:class
+    {
+        private readonly DbventaContext _dbcontext;
+
+        public GenericRepository(DbventaContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        public async Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro)
+        {
+            try
+            {
+                TModel modelo = await _dbcontext.Set<TModel>().FirstOrDefaultAsync(filtro);
+                return modelo;
+            }
+            catch
+            {
+                throw;
+            }
+        } 
+
+        public async Task<TModel> Crear(TModel modelo)
+        {
+            try
+            {
+                _dbcontext.Set<TModel>().Add(modelo);
+                await _dbcontext.SaveChangesAsync();
+                return modelo;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Editar(TModel modelo)
+        {
+            try
+            {
+                _dbcontext.Set<TModel>().Update(modelo);
+                await _dbcontext.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> Eliminar(TModel modelo)
+        {
+            try
+            {
+                _dbcontext.Set<TModel>().Remove(modelo);
+                await _dbcontext.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro = null)
+        {
+            try
+            {
+                IQueryable<TModel> queryModel = filtro == null ? _dbcontext.Set<TModel>(): _dbcontext.Set<TModel>().Where(filtro);
+                return queryModel;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
